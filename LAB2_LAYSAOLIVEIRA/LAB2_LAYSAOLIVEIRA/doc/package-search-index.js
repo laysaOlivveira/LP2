5666a876f6101b8e51809010cbb2126616daecbb
Laysa S Oliveira <141180534+laysaOlivveira@users.noreply.github.com>
2023-09-15 19:24:21 -0300

    Delete LAB2_LAYSAOLIVEIRA/LAB2_LAYSAOLIVEIRA/doc directory

--- LAB2_LAYSAOLIVEIRA/LAB2_LAYSAOLIVEIRA/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"LAB2_LAYSAOLIVEIRA"}];updateSearchResults();
--- LAB2_LAYSAOLIVEIRA/LAB2_LAYSAOLIVEIRA/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"LAB2_LAYSAOLIVEIRA","l":"Coisa"},{"p":"LAB2_LAYSAOLIVEIRA","l":"Descanso"},{"p":"LAB2_LAYSAOLIVEIRA","l":"Disciplina"},{"p":"LAB2_LAYSAOLIVEIRA","l":"RegistroResumos"},{"p":"LAB2_LAYSAOLIVEIRA","l":"RegistroTempoOnline"}];updateSearchResults();
